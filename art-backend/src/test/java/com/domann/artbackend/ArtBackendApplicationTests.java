efa8c39dcc2d43bda2398b7e27a51f7e22386ed6
do-ko <doko@onet.eu>
2025-10-14 18:31:40 +0200

    init backend

---
+++ art-backend/src/test/java/com/domann/artbackend/ArtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domann.artbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
